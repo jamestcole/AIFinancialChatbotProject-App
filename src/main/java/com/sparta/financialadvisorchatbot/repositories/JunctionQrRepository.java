ad51857047b2d7d14898a3dd0ba6cf9fcf44ee1c
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-09-26 14:53:17 +0100

    fix(repositories): Add missing @Repository annotation

--- src/main/java/com/sparta/financialadvisorchatbot/repositories/JunctionQrRepository.java
+++ src/main/java/com/sparta/financialadvisorchatbot/repositories/JunctionQrRepository.java
@@ -3,6 +3,8 @@ package com.sparta.financialadvisorchatbot.repositories;
 import com.sparta.financialadvisorchatbot.entities.JunctionQr;
 import com.sparta.financialadvisorchatbot.entities.JunctionQrId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JunctionQrRepository extends JpaRepository<JunctionQr, JunctionQrId> {
 }
--- src/main/java/com/sparta/financialadvisorchatbot/repositories/QuestionRepository.java
+++ src/main/java/com/sparta/financialadvisorchatbot/repositories/QuestionRepository.java
@@ -2,6 +2,8 @@ package com.sparta.financialadvisorchatbot.repositories;
 
 import com.sparta.financialadvisorchatbot.entities.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 }

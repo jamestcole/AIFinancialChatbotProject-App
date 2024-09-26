3af8226a6edfed527d6169b5e1611fa019d94e96
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-09-26 11:06:44 +0100

    chore(service): Add placeholder FaqService method
    - Placeholder method which will be developed

---
+++ src/main/java/com/sparta/financialadvisorchatbot/service/FaqService.java
@@ -0,0 +1,10 @@
+package com.sparta.financialadvisorchatbot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FaqService {
+    public String findClosestFaq(String userInput) {
+        return "Answer";
+    }
+}

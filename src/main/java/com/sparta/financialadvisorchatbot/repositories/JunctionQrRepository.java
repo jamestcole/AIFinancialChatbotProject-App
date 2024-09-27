342e46ac33b0344cfcdad9ad03a5ed041b19aa5f
Chris Rayner <crayner@spartaglobal.com>
2024-09-27 13:39:41 +0100

    Refactor: Uncommented out classes from last commit

--- src/main/java/com/sparta/financialadvisorchatbot/repositories/JunctionQrRepository.java
+++ src/main/java/com/sparta/financialadvisorchatbot/repositories/JunctionQrRepository.java
@@ -1,10 +1,10 @@
-//package com.sparta.financialadvisorchatbot.repositories;
-//
-//import com.sparta.financialadvisorchatbot.entities.JunctionQr;
-//import com.sparta.financialadvisorchatbot.entities.JunctionQrId;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface JunctionQrRepository extends JpaRepository<JunctionQr, JunctionQrId> {
-//}
+package com.sparta.financialadvisorchatbot.repositories;
+
+import com.sparta.financialadvisorchatbot.entities.JunctionQr;
+import com.sparta.financialadvisorchatbot.entities.JunctionQrId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JunctionQrRepository extends JpaRepository<JunctionQr, JunctionQrId> {
+}
--- src/test/java/com/sparta/financialadvisorchatbot/services/ConversationServiceTest.java
+++ src/test/java/com/sparta/financialadvisorchatbot/services/ConversationServiceTest.java
@@ -1,75 +1,75 @@
-//package com.sparta.financialadvisorchatbot.services;
-//
-//import com.sparta.financialadvisorchatbot.service.ConversationService;
-//import com.sparta.financialadvisorchatbot.service.FaqService;
-//import com.sparta.financialadvisorchatbot.service.OpenAiService;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.io.IOException;
-//
-//import static org.mockito.ArgumentMatchers.anyString;
-//import static org.mockito.Mockito.when;
-//
-//@SpringBootTest
-//@ExtendWith(MockitoExtension.class)
-//public class ConversationServiceTest {
-//
-//    @Mock
-//    OpenAiService openAiService;
-//
-//    @Mock
-//    FaqService faqService;
-//
-//    @InjectMocks
-//    ConversationService conversationService;
-//
-//    @Test //for old code may need rewriting
-//    void testProcessUserInputReturnsGptResponseIfStateIsAwaitingClarification() throws IOException {
-//        String expected = "Test GPT response" + "\nCan I help you with anything else";
-//        when(openAiService.getChatResponse(anyString(), anyString())).thenReturn("Test GPT response");
-//        String actual = conversationService.processUserInput("1","What is a dividend");
-//        Assertions.assertEquals(expected,actual);
-//    }
-//    @Test //for old code may need rewriting
-//    void testProcessUserInputReturnsFaQResponseIfFAQResponseExists() throws IOException {
-//
-//        String expected = "Test FAQ Response" + "\nCan I help you with anything else";
-//        when(faqService.findClosestFaq("How can I save money?")).thenReturn("Test FAQ Response");
-//        String actual = conversationService.processUserInput("1","How can I save money?");
-//        Assertions.assertEquals(expected,actual);
-//
-//    }
-//    @Test //for old code may need rewriting
-//    void testProcessUserInputReturnsGptResponseIfFAQResponseDoesNotExist() throws IOException {
-//        String expected = "Test GPT response" + "\nCan I help you with anything else";
-//        when(openAiService.getChatResponse(anyString(), anyString())).thenReturn("Test GPT response");
-//        String actual = conversationService.processUserInput("1","What is a dividend");
-//        Assertions.assertEquals(expected,actual);
-//    }
-//    @Test
-//    void testGetLatestConversationIdReturnsLatestConversationId(){
-//
-//    }
-//    @Test
-//    void testGetConversationReturnsCurrentConversation(){
-//
-//    }
-//    @Test
-//    void testGetFaqResponseReturnsFaqResponseIfPresent(){
-//
-//    }
-//    @Test
-//    void testGetFaqResponseReturnsGptResponseIfNoMatchToFaq(){
-//
-//    }
-//    @Test
-//    void testGetStartMessageReturnsStartMessage(){
-//
-//    }
-//}
+package com.sparta.financialadvisorchatbot.services;
+
+import com.sparta.financialadvisorchatbot.service.ConversationService;
+import com.sparta.financialadvisorchatbot.service.FaqService;
+import com.sparta.financialadvisorchatbot.service.OpenAiService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class ConversationServiceTest {
+
+    @Mock
+    OpenAiService openAiService;
+
+    @Mock
+    FaqService faqService;
+
+    @InjectMocks
+    ConversationService conversationService;
+
+    @Test //for old code may need rewriting
+    void testProcessUserInputReturnsGptResponseIfStateIsAwaitingClarification() throws IOException {
+        String expected = "Test GPT response" + "\nCan I help you with anything else";
+        when(openAiService.getChatResponse(anyString(), anyString())).thenReturn("Test GPT response");
+        String actual = conversationService.processUserInput("1","What is a dividend");
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test //for old code may need rewriting
+    void testProcessUserInputReturnsFaQResponseIfFAQResponseExists() throws IOException {
+
+        String expected = "Test FAQ Response" + "\nCan I help you with anything else";
+        when(faqService.findClosestFaq("How can I save money?")).thenReturn("Test FAQ Response");
+        String actual = conversationService.processUserInput("1","How can I save money?");
+        Assertions.assertEquals(expected,actual);
+
+    }
+    @Test //for old code may need rewriting
+    void testProcessUserInputReturnsGptResponseIfFAQResponseDoesNotExist() throws IOException {
+        String expected = "Test GPT response" + "\nCan I help you with anything else";
+        when(openAiService.getChatResponse(anyString(), anyString())).thenReturn("Test GPT response");
+        String actual = conversationService.processUserInput("1","What is a dividend");
+        Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    void testGetLatestConversationIdReturnsLatestConversationId(){
+
+    }
+    @Test
+    void testGetConversationReturnsCurrentConversation(){
+
+    }
+    @Test
+    void testGetFaqResponseReturnsFaqResponseIfPresent(){
+
+    }
+    @Test
+    void testGetFaqResponseReturnsGptResponseIfNoMatchToFaq(){
+
+    }
+    @Test
+    void testGetStartMessageReturnsStartMessage(){
+
+    }
+}

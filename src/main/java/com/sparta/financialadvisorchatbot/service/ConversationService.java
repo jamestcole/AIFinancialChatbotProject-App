d7ccfb9563ddf9206f53176583806e5b6d1b32fc
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-09-27 17:50:46 +0100

    feat(conversationService): build save conversation history method

--- src/main/java/com/sparta/financialadvisorchatbot/service/ConversationService.java
+++ src/main/java/com/sparta/financialadvisorchatbot/service/ConversationService.java
@@ -1,12 +1,16 @@
 package com.sparta.financialadvisorchatbot.service;
 
+import com.sparta.financialadvisorchatbot.entities.ConversationHistory;
+import com.sparta.financialadvisorchatbot.entities.ConversationHistoryId;
 import com.sparta.financialadvisorchatbot.entities.ConversationId;
 import com.sparta.financialadvisorchatbot.repositories.ConversationHistoryRepository;
 import com.sparta.financialadvisorchatbot.repositories.ConversationIdRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class ConversationService {
@@ -23,6 +27,19 @@ public class ConversationService {
     public ConversationId startConversation() {
         ConversationId conversationId = new ConversationId();
         return conversationIdRepository.save(conversationId);
+    }
+
+    public void saveConversationHistory(Integer conversationId, String userInput, String botResponse) {
+        ConversationId conversation = conversationIdRepository.findById(conversationId)
+                .orElseThrow(() -> new IllegalArgumentException("Conversation not found."));
+
+        ConversationHistory conversationHistory = new ConversationHistory();
+        conversationHistory.setConversation(conversation);
+        conversationHistory.setInput(userInput);
+        conversationHistory.setResponse(botResponse);
+        conversationHistory.setCreatedAt(LocalDateTime.now());
+
+        conversationHistoryRepository.save(conversationHistory);
     }
 
     public String getStartMessage() {

3a65fd10e43cf58093bb4c829aff37d50a330ef4
Lewis McFarlane <lmcfarlane@spartaglobal.com>
2024-09-27 18:23:49 +0100

    feat(repository): Add repository method to get conversation by conversation id

--- src/main/java/com/sparta/financialadvisorchatbot/repositories/ConversationHistoryRepository.java
+++ src/main/java/com/sparta/financialadvisorchatbot/repositories/ConversationHistoryRepository.java
@@ -3,5 +3,9 @@ package com.sparta.financialadvisorchatbot.repositories;
 import com.sparta.financialadvisorchatbot.entities.ConversationHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConversationHistoryRepository extends JpaRepository<ConversationHistory, Integer> {
+    List<ConversationHistory> findByConversation_ConversationId(Integer conversationId);
+
 }

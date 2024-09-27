be8fd72bc941c5218bd5c5feab50b47466c4048f
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-09-27 10:54:44 +0100

    Update: Updated FaqService to include keyword checking
    - Now returns array list of FAQs that somewhat match the user's input
    - Removed previous methods
    - Replaced standin 'findClosestFaq' method

--- src/main/java/com/sparta/financialadvisorchatbot/service/FaqService.java
+++ src/main/java/com/sparta/financialadvisorchatbot/service/FaqService.java
@@ -5,55 +5,74 @@ import com.sparta.financialadvisorchatbot.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class FaqService {
-    public String findClosestFaq(String userInput) {
-        return "Answer";
+//    public String findClosestFaq(String userInput) {
+//        return "Answer";
+//    }
+//    @Autowired
+//    private QuestionRepository questionRepository;
+//
+//
+//    public List<Question> findFAQ(String userInput) {
+//        return getFAQsByKeyword(findMostCommonKeyword(userInput));
+//    }
+//
+//    // Method to find the most common keyword in a query
+//    public String findMostCommonKeyword(String query) {
+//        // Split the query into individual words
+//        String[] words = query.toLowerCase().split("\\s+");
+//
+//        Map<String, Integer> keywordCount = new HashMap<>();
+//
+//        // Iterate over all words and count their occurrences
+//        for (String word : words) {
+//            keywordCount.put(word, keywordCount.getOrDefault(word, 0) + 1);
+//        }
+//
+//        // Find the most common keyword in the map
+//        String mostCommonKeyword = null;
+//        int maxCount = 0;
+//
+//        for (Map.Entry<String, Integer> entry : keywordCount.entrySet()) {
+//            if (entry.getValue() > maxCount) {
+//                mostCommonKeyword = entry.getKey();
+//                maxCount = entry.getValue();
+//            }
+//        }
+//
+//        return mostCommonKeyword;
+//    }
+//
+//    // Method to get up to 3 FAQs based on the keyword
+//    public List<Question> getFAQsByKeyword(String keyword) {
+//        List<Question> questions = questionRepository.findByKeywordContainingIgnoreCase(keyword);
+//
+//        // Return a maximum of 3 questions
+//        return questions.stream().limit(3).collect(Collectors.toList());
+//    }
+
+    private final QuestionRepository questionRepository;
+
+    public FaqService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
     }
-    @Autowired
-    private QuestionRepository questionRepository;
 
+    public ArrayList<Question> getFAQs(String input) {
+        ArrayList<Question> questions = new ArrayList<>(questionRepository.findAll());
+        String[] words = input.split(" ");
 
-    public List<Question> findFAQ(String userInput) {
-        return getFAQsByKeyword(findMostCommonKeyword(userInput));
-    }
+        ArrayList<Question> result = new ArrayList<>();
 
-    // Method to find the most common keyword in a query
-    public String findMostCommonKeyword(String query) {
-        // Split the query into individual words
-        String[] words = query.toLowerCase().split("\\s+");
-
-        Map<String, Integer> keywordCount = new HashMap<>();
-
-        // Iterate over all words and count their occurrences
-        for (String word : words) {
-            keywordCount.put(word, keywordCount.getOrDefault(word, 0) + 1);
-        }
-
-        // Find the most common keyword in the map
-        String mostCommonKeyword = null;
-        int maxCount = 0;
-
-        for (Map.Entry<String, Integer> entry : keywordCount.entrySet()) {
-            if (entry.getValue() > maxCount) {
-                mostCommonKeyword = entry.getKey();
-                maxCount = entry.getValue();
+        for (Question question : questions) {
+            if (Arrays.stream(words).anyMatch(question.getKeyword()::contains)) {
+                result.add(question);
             }
         }
 
-        return mostCommonKeyword;
-    }
-
-    // Method to get up to 3 FAQs based on the keyword
-    public List<Question> getFAQsByKeyword(String keyword) {
-        List<Question> questions = questionRepository.findByKeywordContainingIgnoreCase(keyword);
-
-        // Return a maximum of 3 questions
-        return questions.stream().limit(3).collect(Collectors.toList());
+        return result.stream().limit(3).collect(Collectors.toCollection(ArrayList::new));
     }
 }

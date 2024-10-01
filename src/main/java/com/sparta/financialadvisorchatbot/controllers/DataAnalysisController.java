f3846e70bdbe8a8aeb12230d802ba6af71dd44fb
Chris Rayner <crayner@spartaglobal.com>
2024-10-01 19:06:20 +0100

    feat(conversations): add error handling for 404 responses from API
    - Implement error handling in `DataAnalysisController` to catch 404 errors using `WebClientResponseException` for `getAllConversations` method.
    - Update Thymeleaf template (`conversations.html`) to display user-friendly error messages when no conversations are found.
    - Add "Back" button in error message view to allow users to navigate back to the full conversation history.
    - Include `isSearch` attribute in the model to distinguish between search and non-search states.
    - Update header text to "Sparta Finance Chatbot Admin" in `conversations.html`.

--- src/main/java/com/sparta/financialadvisorchatbot/controllers/DataAnalysisController.java
+++ src/main/java/com/sparta/financialadvisorchatbot/controllers/DataAnalysisController.java
@@ -6,6 +6,8 @@ import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,27 +37,41 @@ public class DataAnalysisController {
     public String getAllConversations(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "") String keyword, Model model){
 
         ResponseEntity<List<ConversationId>> allConversations;
-        if(keyword.isEmpty()) {
-            allConversations =
-                    webClient
-                            .get()
-                            .uri("/api/sg-financial-chatbot/v1.0/conversations?page=" + page + "&size=" + size)
-                            .retrieve()
-                            .toEntityList(ConversationId.class)
-                            .block();
+        boolean isSearch;
+        try {
+            if (keyword.isEmpty()) {
+                allConversations =
+                        webClient
+                                .get()
+                                .uri("/api/sg-financial-chatbot/v1.0/conversations?page=" + page + "&size=" + size)
+                                .retrieve()
+                                .onStatus(HttpStatusCode::is4xxClientError, ClientResponse::createException)
+                                .toEntityList(ConversationId.class)
+                                .block();
+                isSearch = false;
+            } else {
+                allConversations = webClient
+                        .get()
+                        .uri("/api/sg-financial-chatbot/v1.0/conversations/containing?keyword=" + keyword + "&page=" + page + "&size=" + size)
+                        .retrieve()
+                        .onStatus(HttpStatusCode::is4xxClientError, ClientResponse::createException)
+                        .toEntityList(ConversationId.class)
+                        .block();
+                isSearch = true;
+            }
         }
-        else{
-            allConversations = webClient
-                    .get()
-                    .uri("/api/sg-financial-chatbot/v1.0/conversations/containing?keyword=" + keyword + "&page=" + page + "&size=" + size)
-                    .retrieve()
-                    .toEntityList(ConversationId.class)
-                    .block();
+        catch (WebClientResponseException e) {
+            model.addAttribute("conversations", new ArrayList<ConversationId>());
+            model.addAttribute("error", keyword);
+            model.addAttribute("currentPage", page);
+            model.addAttribute("currentYear", java.time.Year.now().getValue());
+            return "conversations";
         }
 
         model.addAttribute("conversations", allConversations.getBody());
         model.addAttribute("currentPage", page);
         model.addAttribute("currentYear", java.time.Year.now().getValue());
+        model.addAttribute("isSearch", isSearch);
         return "conversations";
 
     }

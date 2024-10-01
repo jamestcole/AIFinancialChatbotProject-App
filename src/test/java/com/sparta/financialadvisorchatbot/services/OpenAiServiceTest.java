c497f2db262ccda647ffdcab392a103b9de34761
KianDoyleSparta <kdoyle@spartaglobal.com>
2024-10-01 11:11:49 +0100

    Update: Updated OpenAiServiceTest class to match new OpenAiService getRequestMethod. All tests pass

--- src/test/java/com/sparta/financialadvisorchatbot/services/OpenAiServiceTest.java
+++ src/test/java/com/sparta/financialadvisorchatbot/services/OpenAiServiceTest.java
@@ -1,6 +1,9 @@
 package com.sparta.financialadvisorchatbot.services;
 
+import com.sparta.financialadvisorchatbot.FinancialAdvisorChatbotApplication;
+import com.sparta.financialadvisorchatbot.entities.ConversationHistory;
 import com.sparta.financialadvisorchatbot.exceptions.ResponseParsingError;
+import com.sparta.financialadvisorchatbot.models.OpenAiResponse;
 import com.sparta.financialadvisorchatbot.service.OpenAiService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,9 +14,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -21,11 +26,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 
-@SpringBootTest
-@EnableConfigurationProperties(value = {OpenAiService.class})
+@SpringBootTest(classes = FinancialAdvisorChatbotApplication.class)
 @TestPropertySource(properties = {
         "openai.api.key=test-api-key",
         "openai.api.endpoint=test-endpoint",
@@ -34,34 +41,58 @@ import static org.mockito.ArgumentMatchers.eq;
 @ExtendWith(MockitoExtension.class)
 public class OpenAiServiceTest {
 
-    @Mock
+    @MockBean
     private RestTemplate restTemplate;
 
-    @InjectMocks
+    @Autowired
     private OpenAiService openAiService;
+
+    OpenAiResponse openAiResponse;
+    OpenAiResponse openAiResponse2;
+
+    @BeforeEach
+    void setUp() {
+
+        OpenAiResponse.Message message = new OpenAiResponse.Message();
+        message.setRole("Financial Advisor Chatbot");
+        message.setContent("Test Bot Response");
+
+        OpenAiResponse.Choice choice = new OpenAiResponse.Choice();
+        choice.setMessage(message);
+
+        openAiResponse = new OpenAiResponse();
+        openAiResponse.setChoices(List.of(choice));
+        openAiResponse2 = new OpenAiResponse();
+        openAiResponse2.setChoices(List.of());
+
+    }
 
     @Test
     public void testGetChatResponseIsSuccessful(){
         String mockResponse = "{\"choices\":[{\"message\":{\"content\":\"Open AI Test Response\"}}]}";
-        String expected = "Open AI Test Response";
+        String expected = "Test Bot Response";
 
         Mockito.when(restTemplate.exchange(any(String.class), any(HttpMethod.class), any(HttpEntity.class), any(Class.class)))
-                .thenReturn(new ResponseEntity<>(mockResponse, HttpStatus.OK));
+                .thenReturn(new ResponseEntity<>(openAiResponse, HttpStatus.OK));
 
         String userMessage = "This is a test message";
-        String actual = openAiService.getResponse(userMessage);
+        ArrayList<ConversationHistory> conversationHistory = new ArrayList<>();
+        int conversationId = 1;
+        String actual = openAiService.getResponse(userMessage, conversationHistory, conversationId);
         Assertions.assertEquals(expected,actual);
     }
     @Test
-    public void testGetChatResponseThrowsUnableToParseExceptionIfUnableToParse(){
+    public void testGetChatResponseThrowsResponseParsingErrorIfNoChoiceFound(){
         String mockResponse = "{\"choices\":[{\"message\":{\"content\":\"Open AI Test Response\"}}}]}";
 
         Mockito.when(restTemplate.exchange(any(String.class), any(HttpMethod.class), any(HttpEntity.class), any(Class.class)))
-                .thenReturn(new ResponseEntity<>(mockResponse, HttpStatus.OK));
+                .thenReturn(new ResponseEntity<>(openAiResponse2, HttpStatus.OK));
 
         String userMessage = "This is a test message";
+        ArrayList<ConversationHistory> conversationHistory = new ArrayList<>();
+        int conversationId = 1;
         Assertions.assertThrows(ResponseParsingError.class, () -> {
-            openAiService.getResponse(userMessage);
+            openAiService.getResponse(userMessage, conversationHistory, conversationId);
         });
     }
     @Test
@@ -70,8 +101,22 @@ public class OpenAiServiceTest {
         Mockito.when(restTemplate.exchange(any(String.class), any(HttpMethod.class), any(HttpEntity.class), any(Class.class)))
                 .thenReturn(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
         String userMessage = "This is a test message";
+        ArrayList<ConversationHistory> conversationHistory = new ArrayList<>();
+        int conversationId = 1;
         Assertions.assertThrows(ResponseParsingError.class, () -> {
-            openAiService.getResponse(userMessage);
+            openAiService.getResponse(userMessage, conversationHistory, conversationId);
+        });
+    }
+    @Test
+    public void testGetChatResponseThrowsErrorIfResponseBodyIsNull(){
+
+        Mockito.when(restTemplate.exchange(any(String.class), any(HttpMethod.class), any(HttpEntity.class), any(Class.class)))
+                .thenReturn(new ResponseEntity<>(null , HttpStatus.OK));
+        String userMessage = "This is a test message";
+        ArrayList<ConversationHistory> conversationHistory = new ArrayList<>();
+        int conversationId = 1;
+        Assertions.assertThrows(ResponseParsingError.class, () -> {
+            openAiService.getResponse(userMessage, conversationHistory, conversationId);
         });
     }
 }
